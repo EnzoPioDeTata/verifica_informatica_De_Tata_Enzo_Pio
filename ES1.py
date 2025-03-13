d4406dea0a75a75b02e5d24b4bd5d52375bc6f91
EnzoPioDeTata <enzop.detata@davincimilazzo.edu.it>
2025-03-13 08:53:43 +0100

    Add files via upload

---
+++ ES1.py
@@ -0,0 +1,11 @@
+L = []
+for elem in range(0,10):
+    L.append(elem)
+print(L)
+
+M = []
+num = 0
+while num < 10:
+    M.append(num)
+    num = num + 1
+print(M)
---
+++ ES2.py
@@ -0,0 +1,6 @@
+L = []
+for elem in range(0,10):
+    x = elem %2==0
+    print("Numeri pari")
+    L.apppend(x)
+print(L)
